e5b62b2160131105c7459e4fc032746d794e9637
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-12-13 12:24:38 +0900

    fix: layout 파일 SSG 코드 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import StyledComponentsRegistry from "../../lib/registry";
 import Nav from "../../components/Nav";
 import "./globals.css";
 
-export const revalidate = 0; // SSG 활성화
-
 export default function RootLayout({
   children,
 }: {
