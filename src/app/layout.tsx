4dac4177c50413b1d8495b2443723240d76e138d
노지용 <115959559+jiyongnoh@users.noreply.github.com>
2024-12-13 10:45:02 +0900

    feat: layout 파일 SSG 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import StyledComponentsRegistry from "../../lib/registry";
 import Nav from "../../components/Nav";
 import "./globals.css";
 
+export const revalidate = 0; // SSG 활성화
+
 export default function RootLayout({
   children,
 }: {
